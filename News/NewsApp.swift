923ceee341984899c4562cda2df2ce93731db483
Kristina Yaroshenko <kris.jaroshenko09@gmail.com>
2025-05-24 23:17:45 +0300

    Initial Commit

---
+++ News/NewsApp.swift
@@ -0,0 +1,17 @@
+//
+//  NewsApp.swift
+//  News
+//
+//  Created by Kristina Yaroshenko on 24.05.2025.
+//
+
+import SwiftUI
+
+@main
+struct NewsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
